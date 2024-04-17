72ffe1ce10af584fb1463dca4e0821f8cd8fd56d
Chao Ming Huang <mingqed@gmail.com>
2024-04-17 11:25:20 +0800

    Implement `HTTPClient`

---
+++ PokedexDemo/NetworkModule/HTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  HTTPClient.swift
+//  PokedexDemo
+//
+//  Created by 黃昭銘 on 2024/4/17.
+//
+
+import Foundation
+
+class HTTPClient {
+    
+    let urlSession = URLSession.shared
+    
+    
+    func request(with requestType: RequestType, completion: @escaping (Data) -> Void) {
+        urlSession.dataTask(with: requestType.getURLRequest()) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if let error = error {
+                print(error)
+                return
+            }
+            if let reponse = response as? HTTPURLResponse {
+                print(reponse.statusCode)
+            }
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                completion(data)
+            }
+        }.resume()
+    }
+}
