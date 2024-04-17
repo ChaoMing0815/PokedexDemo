5d5074caf63fc500be5eb64952a5d38b5ddb386f
Chao Ming Huang <mingqed@gmail.com>
2024-04-17 10:01:12 +0800

    Initial Commit

---
+++ PokedexDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexDemo
+//
+//  Created by 黃昭銘 on 2024/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
