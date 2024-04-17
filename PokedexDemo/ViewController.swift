613c685bd9fe6b6fdbb08e6304e8f598af4e9df7
Chao Ming Huang <mingqed@gmail.com>
2024-04-17 15:54:45 +0800

    Back to main thread after receiving response from `AllPokemonListService`

--- PokedexDemo/ViewController.swift
+++ PokedexDemo/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let allPokemonListViewModel = AllPokemonListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        allPokemonListViewModel.delegate = self
+        allPokemonListViewModel.loadAllPokemonList()
     }
 
 
 }
 
+extension ViewController: AllPokemonListViewModelDelegate {
+    func allPokemonListViewModel(_ allPokemonListViewModel: AllPokemonListViewModel, allPokemonListDidUpdate allPokemonList: AllPokemonList) {
+        print(allPokemonList)
+    }
+    
+    func allPokemonListViewModel(_ allPokemonListViewModel: AllPokemonListViewModel, allPokemonListErrorDidUpdate error: AllPokemonListServiceError) {
+        print(error)
+    }
+    
+    
+}
