bd239680f79e7a5048fa6c84c262d55889ef5879
Chao Ming Huang <mingqed@gmail.com>
2024-04-17 14:27:19 +0800

    Implement `AllPokemonListDTO`

---
+++ PokedexDemo/AllPokemonListPage/ModelLayer/AllPokemonListDTO.swift
@@ -0,0 +1,28 @@
+//
+//  AllPokemonListDTO.swift
+//  PokedexDemo
+//
+//  Created by 黃昭銘 on 2024/4/17.
+//
+
+import Foundation
+
+struct AllPokemonListDTO: Codable {
+    struct Result: Codable {
+        let name: String
+        let url: String
+
+        enum CodingKeys: String, CodingKey {
+            case name
+            case url
+        }
+    }
+    
+    let count: Int
+    let results: [Result]
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case results
+    }
+}
