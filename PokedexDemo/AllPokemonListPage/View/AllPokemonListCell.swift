91de41343aa5be1e571bfff79362bd4c9c4846bd
Chao Ming Huang <mingqed@gmail.com>
2024-04-30 12:14:10 +0800

    Modify `AllPokemonListCell`

--- PokedexDemo/AllPokemonListPage/View/AllPokemonListCell.swift
+++ PokedexDemo/AllPokemonListPage/View/AllPokemonListCell.swift
@@ -8,19 +8,34 @@
 import UIKit
 
 class AllPokemonListCell: UITableViewCell {
+    
+    lazy var pokemonNameCellBackground = makeCellBackgroundView()
     lazy var nameLabel = makeNameLabel()
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         setupLayout()
+        
+        contentView.backgroundColor = .clear
+        pokemonNameCellBackground.backgroundColor = .clear
+        nameLabel.text = nameLabel.text?.capitalized
+        nameLabel.font = .boldSystemFont(ofSize: 22)
     }
 }
 
 extension AllPokemonListCell {
     fileprivate func setupLayout() {
-        contentView.addSubview(nameLabel)
-        nameLabel.constraint(top: contentView.snp.top, bottom: contentView.snp.bottom, left: contentView.snp.left)
+        contentView.addSubview(pokemonNameCellBackground)
+        pokemonNameCellBackground.addSubview(nameLabel)
+        pokemonNameCellBackground.constraint(top: contentView.snp.top, bottom: contentView.snp.bottom, left: contentView.snp.left)
+        nameLabel.constraint(top: pokemonNameCellBackground.snp.top, bottom: pokemonNameCellBackground.snp.bottom, left: pokemonNameCellBackground.snp.left, padding: .init(top: 8, left: 16, bottom: 8, right: 0))
     }
+    
+    fileprivate func makeCellBackgroundView() -> UIView {
+        let view = UIView()
+        return view
+    }
+    
     fileprivate func makeNameLabel() -> UILabel {
         let nameLabel = UILabel()
         return nameLabel
