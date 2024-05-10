d5356fcc37f9577bc50be43651f208b4278618ee
Chao Ming Huang <mingqed@gmail.com>
2024-05-10 22:22:09 +0800

    Modify `AllPokemonListViewController`, change UITableView into UICollectionView, implement `makeCollectionView` factory method

--- PokedexDemo/AllPokemonListPage/ViewController/AllPokemonListViewController.swift
+++ PokedexDemo/AllPokemonListPage/ViewController/AllPokemonListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class AllPokemonListViewController: UIViewController {
     // MARK: - stored properties
     let viewModel = AllPokemonListViewModel()
-    lazy var pokemonListTableView = makeTableView()
+    lazy var pokemonListCollectionView = makeCollectionView()
     lazy var indicatorFooterView = makeIndicatorView()
     var indicator: UIActivityIndicatorView {
         return indicatorFooterView.indicator
@@ -24,19 +24,19 @@ class AllPokemonListViewController: UIViewController {
         super.viewDidLoad()
         title = "Pokeomn List"
         
-        view.addSubview(pokemonListTableView)
-        pokemonListTableView.constraint(top: view.safeAreaLayoutGuide.snp.top, bottom: view.safeAreaLayoutGuide.snp.bottom, left: view.snp.left, right: view.snp.right)
+        view.addSubview(pokemonListCollectionView)
+        pokemonListCollectionView.constraint(top: view.safeAreaLayoutGuide.snp.top, bottom: view.safeAreaLayoutGuide.snp.bottom, left: view.snp.left, right: view.snp.right)
         
         viewModel.delegate = self
         viewModel.loadAllPokemonList()
     }
     
     func makeIndicatorView() -> (footerView: UIView, indicator: UIActivityIndicatorView) {
-        let footerView = UIView(frame: .init(x: 0, y: 0, width: pokemonListTableView.frame.width, height: 50))
+        let footerView = UIView(frame: .init(x: 0, y: 0, width: pokemonListCollectionView.frame.width, height: 50))
         let indicator = UIActivityIndicatorView(style: .medium)
         indicator.center = footerView.center
         footerView.addSubview(indicator)
-        pokemonListTableView.tableFooterView = footerView
+        
         return (footerView, indicator)
     }
 }
@@ -51,7 +51,7 @@ extension AllPokemonListViewController: AllPokemonListViewModelDelegate {
     }
     
     func allPokemonListViewModel(_ allPokemonListViewModel: AllPokemonListViewModel, allPokemonListDidUpdate allPokemonList: AllPokemonList) {
-        pokemonListTableView.reloadData()
+        pokemonListCollectionView.reloadData()
         viewModel.isLoadingAndPresentingNewPokemonList = false
     }
     
@@ -61,55 +61,66 @@ extension AllPokemonListViewController: AllPokemonListViewModelDelegate {
 }
 
 extension AllPokemonListViewController {
-    func makeTableView() -> UITableView {
-        let tableView = UITableView()
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(AllPokemonListCell.self, forCellReuseIdentifier: String(describing: AllPokemonListCell.self))
-        return tableView
+    func makeCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        return collectionView
     }
 }
 
-extension AllPokemonListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.allPokemonListCellModels.count
+extension AllPokemonListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: AllPokemonListCell.self), for: indexPath) as! AllPokemonListCell
-        let cellModel = viewModel.allPokemonListCellModels[indexPath.row]
-        cell.configureCell(with: cellModel)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: AllPokemonCollectionCell.self), for: indexPath) as! AllPokemonCollectionCell
         return cell
     }
-    
-    // 當 tableView 滑到底時
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        // 取得當前 tableView 的最後一個 section 的最後一個 row (tableView 的最底下的 row)
-        let lastSectionIndex = tableView.numberOfSections - 1
-        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
-        // 判斷最後一個 cell 是否有要被顯示
-        if indexPath.section == lastSectionIndex && indexPath.row == lastRowIndex {
-            viewModel.loadAllPokemonList()
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return footerView
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 50
-    }
 }
 
-extension AllPokemonListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        viewModel.setupPokemonNameForInfoPage(with: indexPath)
-        let pokemonInfoPageViewController = PokemonInfoPageViewController()
-        pokemonInfoPageViewController.dataSource = self
-        navigationController?.pushViewController(pokemonInfoPageViewController, animated: true)
-    }
-}
+//extension AllPokemonListViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.allPokemonListCellModels.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: AllPokemonListCell.self), for: indexPath) as! AllPokemonListCell
+//        let cellModel = viewModel.allPokemonListCellModels[indexPath.row]
+//        cell.configureCell(with: cellModel)
+//        return cell
+//    }
+    
+    // 當 tableView 滑到底時
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        // 取得當前 tableView 的最後一個 section 的最後一個 row (tableView 的最底下的 row)
+//        let lastSectionIndex = tableView.numberOfSections - 1
+//        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
+//        // 判斷最後一個 cell 是否有要被顯示
+//        if indexPath.section == lastSectionIndex && indexPath.row == lastRowIndex {
+//            viewModel.loadAllPokemonList()
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+//        return footerView
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return 50
+//    }
+//}
+
+//extension AllPokemonListViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        viewModel.setupPokemonNameForInfoPage(with: indexPath)
+//        let pokemonInfoPageViewController = PokemonInfoPageViewController()
+//        pokemonInfoPageViewController.dataSource = self
+//        navigationController?.pushViewController(pokemonInfoPageViewController, animated: true)
+//    }
+//}
 
 extension AllPokemonListViewController: PokemonInfoPageViewControllerDataSource {
     var pokemonName: String {
