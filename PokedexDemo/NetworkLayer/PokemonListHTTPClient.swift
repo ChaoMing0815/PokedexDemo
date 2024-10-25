d55603f9e153ee028129c5994b8d65dc3993c0eb
Chao Ming Huang <mingqed@gmail.com>
2024-10-25 11:43:22 +0800

    Modify 'PokemonListHTTPClient'

--- PokedexDemo/NetworkLayer/PokemonListHTTPClient.swift
+++ PokedexDemo/NetworkLayer/PokemonListHTTPClient.swift
@@ -8,27 +8,35 @@
 import Foundation
 
 class PokemonListHTTPClient: HTTPClient {
-    fileprivate var offset: Int = 0
+    fileprivate var offset: Int
     fileprivate let limit: Int
+    fileprivate let startID: Int
+    fileprivate let endID: Int
     
-    init(limit: Int = 30) {
+    init(limit: Int = 30, startID: Int, endID: Int) {
         self.limit = limit
+        self.startID = startID
+        self.endID = endID
+        self.offset = startID - 1
+        super.init()
     }
     
-    func requestAllPokemonList(completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void) {
-        request(with: allPokemonListRequestType, completion: completion)
+    func requestPokemonList(completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void) {
+        request(with: pokemonListRequestType, completion: completion)
     }
     
     func updateOffset() {
         offset += limit
+        if offset > endID {
+            offset = endID
+        }
     }
-    
     
 }
 
 extension PokemonListHTTPClient {
     // https://pokeapi.co/api/v2/pokemon?limit=1302
-    var allPokemonListRequestType: RequestType {
+    var pokemonListRequestType: RequestType {
         return RequestType.init(httpMethod: .GET, domainURL: .init(string: "https://pokeapi.co/api/v2")!, path: "/pokemon", queryItems: [.init(name: "limit", value: "\(limit)"), .init(name: "offset", value: "\(offset)")])
     }
 }
