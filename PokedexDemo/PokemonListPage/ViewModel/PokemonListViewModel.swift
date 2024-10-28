e8789148395c0a0785771501219afeccbe088790
Chao Ming Huang <mingqed@gmail.com>
2024-10-28 22:31:26 +0800

    Modify 'PokemonListViewModel' for generation pokemon loading and pagination

--- PokedexDemo/PokemonListPage/ViewModel/PokemonListViewModel.swift
+++ PokedexDemo/PokemonListPage/ViewModel/PokemonListViewModel.swift
@@ -24,33 +24,45 @@ class PokemonListViewModel {
     var cellModels = [PokemonListCellModel]()
     var pokemonIDForInfoPage: String?
     var isLoadingAndPresentingNewPokemonList = false
+    var isLastPageLoaded = false
     
     init() {
        
     }
     
     func loadPokemonListAndImage() {
-        if isLoadingAndPresentingNewPokemonList {
-            return
-        }
+        // 確保當前並沒有在執行載入新的寶可夢列表，且尚未達到當前世代最後一批寶可夢
+        guard !isLoadingAndPresentingNewPokemonList, !isLastPageLoaded else { return }
+        
         delegate?.pokemonListViewModel(self, willLoadNewPokemonList: true)
         isLoadingAndPresentingNewPokemonList = true
         guard let useCase = self.useCase else { return }
         useCase.loadPokemonListAndImage(with: selectedGeneration) { [weak self] result in
             guard let self else { return }
-            switch result {
-            case let .success(pokemonList):
-                let pokemonInfos = pokemonList.pokemonInfos
-                self.cellModels = pokemonInfos.map({ PokemonListCellModel(id: $0.id, name: $0.name, imageData: $0.imageData)
-                })
-                self.cellModels.sort(by: { Int($0.id)! < Int($1.id)! })
-                self.delegate?.pokemonListViewModel(self, cellModelsDidUpdate: self.cellModels)
-                self.delegate?.pokemonListViewModel(self, didLoadNewPokemonList: true)
-                self.isLoadingAndPresentingNewPokemonList = false
-                
-            case let .failure(error):
-                self.isLoadingAndPresentingNewPokemonList = false
-                self.delegate?.pokemonListViewModel(self, pokemonListErrorDidUpdate: error)
+            
+            DispatchQueue.main.async {
+                switch result {
+                case let .success(pokemonList):
+                    let pokemonInfos = pokemonList.pokemonInfos
+                    // 使用append將新載入的寶可夢資料正確加入'cellModels'中，而不是透過賦值的覆蓋原來的'cellModels'，以必免不斷觸發'loadNewPokemons'
+                    self.cellModels.append(contentsOf: pokemonInfos.map { PokemonListCellModel(id: $0.id, name: $0.name, imageData: $0.imageData)
+                    })
+                    self.cellModels.sort(by: { Int($0.id)! < Int($1.id)! })
+                    if let lastID = Int(self.cellModels.last?.id ?? ""),
+                       lastID > self.selectedGeneration.range.endID {
+                        // 移除所有ID超過endID的寶可夢，確保沒有其他世代的寶可夢誤載至當前世代cellModels中
+                        self.cellModels.removeAll { Int($0.id)! > self.selectedGeneration.range.endID }
+                        // 當已經載入到當前世代的endID寶可夢時代表進入最後一頁加載，因此變更'isLastPageLoaded'為true，使分頁加載功能停止繼續觸發。
+                        self.isLastPageLoaded = true
+                    }
+                    self.delegate?.pokemonListViewModel(self, cellModelsDidUpdate: self.cellModels)
+                    self.delegate?.pokemonListViewModel(self, didLoadNewPokemonList: true)
+                    self.isLoadingAndPresentingNewPokemonList = false
+                    
+                case let .failure(error):
+                    self.isLoadingAndPresentingNewPokemonList = false
+                    self.delegate?.pokemonListViewModel(self, pokemonListErrorDidUpdate: error)
+                }
             }
         }
     }
@@ -71,6 +83,7 @@ class PokemonListViewModel {
 extension PokemonListViewModel {
     func loadNewPokemons(withIndexPath indexPath: IndexPath, lastSectionIndex: Int, lastItemIndex: Int) {
         if indexPath.section == lastSectionIndex && indexPath.item == lastItemIndex {
+            // 確保沒有正在加載新的寶可夢
             if !self.isLoadingAndPresentingNewPokemonList {
                 self.loadPokemonListAndImage()
             }
